cec1dbc67bf86b412cc51d3e3a2b28c0d5605b5a
JoaoDelgado12 <joaodelgadodasilva@gmail.com>
2026-05-11 17:11:34 -0300

    banco do pipeline modificado

--- src/main/webapp/db/init.sql
+++ src/main/webapp/db/init.sql
@@ -1,83 +1,134 @@
 CREATE SCHEMA IF NOT EXISTS estoque_db DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-
 USE estoque_db ;
 
-CREATE TABLE IF NOT EXISTS usuario(
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  nome VARCHAR(100),
-  sobrenome VARCHAR(100),
-  dtaNasc INT,
-  CONSTRAINT fk_dataNasc FOREIGN KEY (dtaNasc) REFERENCES dta_nascimento_usuario(dta_nascimento),
-  sexo INT DEFAULT 0.
-  CONSTRAINT fk_sexo FOREIGN KEY (sexo) REFERENCES sexo_usuario(sexo),
-  cpf VARCHAR(11) UNIQUE,
-  cep INT,
-  CONSTRAINT fk_cepUsuario FOREIGN KEY (cep) REFERENCES cep_usuario(cep),
-  email VARCHAR(120),
-  telefone VARCHAR(11)
-  )
+CREATE TABLE IF NOT EXISTS funcao_perfilUsuario(
+  funcao VARCHAR(100) PRIMARY KEY
+);
 
-CREATE TABLE IF NOT EXISTS dta_nascimento_usuario(
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  dta_nascimento DATE NOT NULL
-)
+CREATE TABLE IF NOT EXISTS grupoPermissao_perfilUsuario(
+  grupoPermissao VARCHAR(100) PRIMARY KEY
+);
 
-CREATE TABLE IF NOT EXISTS sexo_usuario(
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  sexo VARCHAR(15) NOT NULL
-)
+CREATE TABLE IF NOT EXISTS perfilUsuario(
+  id int AUTO_INCREMENT PRIMARY KEY,
+  usuario VARCHAR(100) UNIQUE, 
+  senha VARCHAR(100),
+  funcao VARCHAR(100),
+  CONSTRAINT fk_funcao_perfil FOREIGN KEY (funcao) REFERENCES funcao_perfilUsuario(funcao) on update cascade on delete restrict,
+  grupoPermissao VARCHAR(100),
+  CONSTRAINT fk_grupoPermissao_perfil FOREIGN KEY (grupoPermissao) REFERENCES grupoPermissao_perfilUsuario(grupoPermissao) on update cascade on delete restrict
+);
 
-CREATE TABLE IF NOT EXISTS cep_usuario(
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  cep INT NOT NULL,
-  CONSTRAINT fk_cep FOREIGN KEY (cep) REFERENCES cep(cep),
-  estado INT NOT NULL,
-  CONSTRAINT fk_estado FOREIGN KEY (estado) REFERENCES estado(estado),
-  bairro VARCHAR(30) NOT NULL,
-  cidade INT NOT NULL,
-  CONSTRAINT fk_cidade FOREIGN KEY (cidade) REFERENCES cidade(cidade),
-  numero VARCHAR(5),
-  complemento VARCHAR(30)
-)
 
-CREATE TABLE IF NOT EXISTS cep(
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  cep VARCHAR(15) NOT NULL
-)
 CREATE TABLE IF NOT EXISTS estado(
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  estado VARCHAR(15) NOT NULL
-)
-CREATE TABLE IF NOT EXISTS cidade(
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  cidade VARCHAR(15) NOT NULL
-)
+  estado VARCHAR(15) PRIMARY KEY
+);
 
-CREATE TABLE IF NOT EXISTS perfil_colaborador(
-  id INT PRIMARY KEY,
-  CONSTRAINT fk_perfilColaborador FOREIGN KEY id REFERENCES usuario(id) on cascade update on cascade delete,
-  usuario VARCHAR(100) NOT NULL,
-  senha VARCHAR(100) NOT NULL,
-  funcao INT DEFAULT 0,
-  CONSTRAINT fk_funcao_perfil_colaborador FOREIGN KEY id REFERENCES funcao_perfil_colaborador(id),
-  grupoPermissao INT DEFAULT 0,
-  CONSTRAINT fk_funcao_perfil_colaborador FOREIGN KEY id REFERENCES grupoPermissao_perfil_colaborador(id),
-  ativo enum['0','1']
-)
+CREATE TABLE IF NOT EXISTS  cidade(
+  cidade VARCHAR(50) PRIMARY KEY
+);
 
-CREATE TABLE IF NOT EXISTS funcao_perfil_colaborador(
+CREATE TABLE IF NOT EXISTS cepUsuario(
   id INT AUTO_INCREMENT PRIMARY KEY,
-  funcao VARCHAR(20) NOT NULL
-)
+  CONSTRAINT fk_idCepUsuario_perfilUsuario FOREIGN KEY (id) REFERENCES perfilUsuario(id) on update cascade on delete cascade,
+  cep VARCHAR(11),
+  estado VARCHAR(15),
+  CONSTRAINT fk_estado_cepUsuario FOREIGN KEY (estado) REFERENCES estado(estado) on update cascade on delete cascade,
+  cidade VARCHAR(50),
+  CONSTRAINT fk_cidade_cepUsuario FOREIGN KEY (cidade) REFERENCES cidade(cidade) on update cascade on delete cascade,
+  logradouro VARCHAR(50),
+  numero int,
+  barrio VARCHAR(50),
+  complemento VARCHAR(150)
+);
 
-CREATE TABLE IF NOT EXISTS grupoPermissao_perfil_colaborador(
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  grupoPermissao VARCHAR(20) NOT NULL
-)
+CREATE TABLE IF NOT EXISTS  dadosUsuario(
+  id int AUTO_INCREMENT PRIMARY KEY,
+  CONSTRAINT fk_idDadosUsuario_perfilUsuario FOREIGN KEY (id) REFERENCES perfilUsuario(id) on delete cascade,
+  nome VARCHAR(100), 
+  sobrenome VARCHAR(100),
+  dtaNascimento DATE,
+  sexo VARCHAR(10),
+  cpf VARCHAR(11) UNIQUE,
+  email VARCHAR(120),
+  telefone VARCHAR(13)
+);
 
-CREATE TABLE IF NOT EXISTS perfil_cliente(
-  id INT PRIMARY KEY,
-  CONSTRAINT fk_perfilCliente FOREIGN KEY id REFERENCES perfil_colaborador() on cascade update on cascade delete,
-  usuario VARCHAR(100) NOT NULL,
-  senha VARCHAR(100) NOT NULL,
-)
+CREATE TABLE IF NOT EXISTS EmpresaNome(
+	empresaNome VARCHAR(120) PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS Fornecedor(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	nome VARCHAR(120),
+	telefone VARCHAR(13),
+	empresaNome VARCHAR(120),
+	CONSTRAINT fk_empresaNome_fornecedor FOREIGN KEY (empresaNome) REFERENCES EmpresaNome(empresaNome) on update cascade on delete restrict
+);
+
+CREATE TABLE IF NOT EXISTS Marca(
+	marca VARCHAR(120) PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS DataPrecoVenda(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	dataPrecoVenda DATE,
+	precoVendaUni DECIMAL(10,2)
+);
+
+CREATE TABLE IF NOT EXISTS Produto(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	nome VARCHAR(120),
+	quantidade INT,
+	precoVendaUni DECIMAL(10,2),
+	marca VARCHAR(120),
+	CONSTRAINT fk_marca_Produto FOREIGN KEY (marca) REFERENCES Marca(marca) on update cascade on delete restrict,
+	fornecedor INT,
+	CONSTRAINT fk_fornecedor_Produto FOREIGN KEY (fornecedor) REFERENCES Fornecedor(id) on update cascade
+);
+
+CREATE TABLE IF NOT EXISTS HistEstoque(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	produto INT,
+	CONSTRAINT fk_produto_HistEstoque FOREIGN KEY (produto) REFERENCES Produto(id) on update cascade,
+	quantidade INT,
+	dataPrecoCompra DATE,
+	precoCompraUni DECIMAL(10,2),
+	precoCompraTotal DECIMAL(10,2)
+);
+
+CREATE TABLE IF NOT EXISTS Venda(
+	id int AUTO_INCREMENT PRIMARY KEY,
+	precoTotal DECIMAL(10,2),
+	desconto DECIMAL(10,2),
+	dataVenda DATE
+);
+
+CREATE TABLE IF NOT EXISTS Cupom(
+	id int AUTO_INCREMENT PRIMARY KEY,
+	descontoPorcent DECIMAL(10,2),
+	descontoReal DECIMAL(10,2),
+	usoQuantidadeMax INT
+);
+
+CREATE TABLE IF NOT EXISTS ItensVenda(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	quantidade INT,
+	dataVenda DATE,
+	precoTotal DECIMAL(10,2),
+	desconto Decimal(10,2),
+	produto INT,
+	CONSTRAINT fk_produto_ItensVenda FOREIGN KEY (produto) REFERENCES Produto(id) on update cascade,
+	venda int,
+	CONSTRAINT fk_venda_ItensVenda FOREIGN KEY (venda) REFERENCES Venda(id) on update cascade on delete cascade
+);
+
+CREATE TABLE IF NOT EXISTS CupomVenda(
+	vendaID int,
+	CONSTRAINT fk_vendaID_CupomVenda FOREIGN KEY (vendaID) REFERENCES Venda(id) on update cascade,
+	cupomID int,
+	CONSTRAINT fk_cupomID_CupomVenda FOREIGN KEY (cupomID) REFERENCES Cupom(id) on update cascade,
+	PRIMARY KEY (vendaID, cupomID)
+);
+
+
